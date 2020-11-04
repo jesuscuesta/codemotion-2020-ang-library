4d96ce8034bdcfd930ab23812469b3e06adc7d0e
Jesus Cuesta <jesus@jesuscuesta.net>
2020-11-04 01:17:26 +0100

    replace

---
+++ esm2015/anime-ejemplo.js
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public-api';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5pbWUtZWplbXBsby5qcyIsInNvdXJjZVJvb3QiOiJDOi9wcm95ZWN0b3MvY29kZW1vdGlvbi9jb2RlbW90aW9uLTIwMjAvcHJvamVjdHMvYW5pbWUvc3JjLyIsInNvdXJjZXMiOlsiYW5pbWUtZWplbXBsby50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7R0FFRztBQUVILGNBQWMsY0FBYyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL3B1YmxpYy1hcGknO1xuIl19
---
+++ fesm2015/anime-ejemplo.js
@@ -0,0 +1,50 @@
+import { ɵɵdefineInjectable, Injectable, Component, NgModule } from '@angular/core';
+
+class AnimeService {
+    constructor() { }
+}
+AnimeService.ɵprov = ɵɵdefineInjectable({ factory: function AnimeService_Factory() { return new AnimeService(); }, token: AnimeService, providedIn: "root" });
+AnimeService.decorators = [
+    { type: Injectable, args: [{
+                providedIn: 'root'
+            },] }
+];
+AnimeService.ctorParameters = () => [];
+
+class AnimeComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+}
+AnimeComponent.decorators = [
+    { type: Component, args: [{
+                selector: 'anime-anime',
+                template: `
+    <p>
+      anime works!
+    </p>
+  `
+            },] }
+];
+AnimeComponent.ctorParameters = () => [];
+
+class AnimeModule {
+}
+AnimeModule.decorators = [
+    { type: NgModule, args: [{
+                declarations: [AnimeComponent],
+                imports: [],
+                exports: [AnimeComponent]
+            },] }
+];
+
+/*
+ * Public API Surface of anime
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { AnimeComponent, AnimeModule, AnimeService };
+//# sourceMappingURL=anime-ejemplo.js.map
